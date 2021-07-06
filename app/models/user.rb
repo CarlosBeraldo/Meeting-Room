a06065e27f93b2095dec90dbf748e0dd65c2d6e9
Carlos Beraldo <carlosberaldo06@gmail.com>
2021-07-06 14:37:55 -0300

    Validações se 'username' caso ja exista retorna erro

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :meetings
+  validates :username, presence: true
+	validates :username, uniqueness: true
 end
